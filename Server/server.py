bb0cbed0696a6f9105882630b11aaa82c505574b
Liramy <lirammash1@gmail.com>
2023-10-28 21:43:15 +0300

    Make server and socket for server. Create username and password .txt file at start. Multi-client processing for many clients. Add set amount of interactions: 0 interactions - user gives username and password. 1 interactions - user gives encrypted image. 2 interactions - undecided.

--- Server/server.py
+++ Server/server.py
@@ -1,3 +1,12 @@
+import io
+
+import select
+import socket
+import threading
+import os
+
+from PIL import Image
+
 """
   Adding employees:
   - When someone enters your group in the client GUI menu press Add Employee.
@@ -18,4 +27,67 @@
   - If they match the server accepts the client and gives confirmation.
 """
 
-import socket
+
+def on_new_client(client_socket: socket.socket, addr):
+    print(f"connection established with {addr}")
+
+
+def create_user(raw_data: bytes):
+    raw_data = raw_data.decode()
+    username, password = raw_data.split('/***-***/')
+
+    file = open('../Server/Users.txt', 'r')
+    file_data = file.read()
+    file.close()
+
+    file = open('../Server/Users.txt', 'w')
+    file.write(file_data + f'{username}-:-{password}\n')
+    file.close()
+
+
+def create_dir():
+    users_exists = os.path.exists('../Server/Users.txt')
+    if not users_exists:
+        os.makedirs('../Server/Users.txt')
+
+
+def load_image(img_bytes):
+    image = Image.open(io.BytesIO(img_bytes))
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+IP = '127.0.0.1'
+PORT = 8000
+
+create_dir()
+
+client_dict: {any: socket.socket} = {}
+client_interaction: {socket.socket: int} = {}
+
+server_socket.bind((IP, PORT))
+
+server_socket.listen()
+
+while True:
+    client_socket, client_addr = server_socket.accept()
+    client_dict[client_addr] = client_socket
+    client_interaction[client_socket] = 0
+
+    on_welcome = threading.Thread(target=on_new_client, args=(client_socket, client_addr,))
+    on_welcome.start()
+
+    rlist, wlist, _ = select.select([], [], [], __timeout=10)
+
+    for readable in rlist:
+        data = readable.recv(2048)
+
+        # When interaction is 0, client sends username and password
+        if client_interaction[readable] == 0:
+            welcome_user = threading.Thread(target=create_user, args=(data,))
+            welcome_user.start()
+
+        if client_interaction[readable] == 1:
+            threading.Thread(target=load_image, args=(data,))
+
+        if client_interaction[readable] == 2:
+            pass
